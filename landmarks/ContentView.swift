b29180add90174cbdea7facace19cf1cf786c3c6
esau-morais <esaumorais7@gmail.com>
2023-09-01 20:54:10 -0300

    Initial Commit

---
+++ landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  landmarks
+//
+//  Created by PDS on 01/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by PDS on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
